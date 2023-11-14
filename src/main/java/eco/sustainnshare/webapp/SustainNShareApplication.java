6ede2745665c455071b6d16e92db8ae9cdd82837
Lucas Reynolds <lucaslegolover06@gmail.com>
2023-11-13 19:00:31 -0700

    commit

---
+++ src/main/java/eco/sustainnshare/webapp/SustainNShareApplication.java
@@ -0,0 +1,13 @@
+package eco.sustainnshare.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SustainNShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SustainNShareApplication.class, args);
+	}
+
+}
