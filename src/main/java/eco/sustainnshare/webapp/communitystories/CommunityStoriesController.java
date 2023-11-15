f64b9b99342bed0b1739a47bf26c1fae4b171694
Emily Reynolds <fireandicefangirl@gmail.com>
2023-11-14 20:51:50 -0700

    feat: link navbar to pages

--- src/main/java/eco/sustainnshare/webapp/communitystories/CommunityStoriesController.java
+++ src/main/java/eco/sustainnshare/webapp/communitystories/CommunityStoriesController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CommunityStoriesController {
+
     @GetMapping("/community-stories")
     public String communityStories(){
         return "community-stories";
