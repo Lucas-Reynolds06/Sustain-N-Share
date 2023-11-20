7b4fe0b852805aa784909a96bb6aff00a3700439
emalyjoann <fireandicefangirl@gmail.com>
2023-11-19 20:53:45 -0700

    feat: add sign up

--- src/main/java/eco/sustainnshare/webapp/services/UsersService.java
+++ src/main/java/eco/sustainnshare/webapp/services/UsersService.java
@@ -1,7 +1,9 @@
 package eco.sustainnshare.webapp.services;
 
+import eco.sustainnshare.webapp.dto.UserDto;
 import eco.sustainnshare.webapp.entity.Users;
 
 public interface UsersService {
     Users getUserByID(int id);
+    Users createUser(UserDto userDto);
 }
