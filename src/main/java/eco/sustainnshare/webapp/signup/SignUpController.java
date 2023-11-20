db3583ef6ca1a52bb4d5e56400740d83b1447b0e
emalyjoann <fireandicefangirl@gmail.com>
2023-11-19 20:53:45 -0700

    feat: add sign up

--- src/main/java/eco/sustainnshare/webapp/signup/SignUpController.java
+++ src/main/java/eco/sustainnshare/webapp/signup/SignUpController.java
@@ -1,12 +1,33 @@
 package eco.sustainnshare.webapp.signup;
 
+import eco.sustainnshare.webapp.dto.UserDto;
+import eco.sustainnshare.webapp.services.UsersService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class SignUpController {
+
+    private final UsersService userService;
+
+    @PostMapping("/sign-up")
+    public String signUp(Model model, UserDto userDto) {
+        try {
+            var user = userService.createUser(userDto);
+            return "signup-success";
+        } catch(RuntimeException e) {
+            model.addAttribute("error", e.getMessage());
+            return "signup-failure";
+        }
+    }
+
     @GetMapping("/sign-in")
-    public String signInOrUp(){
+    public String signInOrUp(Model model){
+        model.addAttribute("user", new UserDto());
         return "sign-in-or-up";
     }
 }
